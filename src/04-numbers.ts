f7d82d2c1deaca2b75632dfd6e3aa810d5aa7493
Javier Cepeda <erjavierc@gmail.com>
2022-08-09 09:08:24 -0400

    typescript

--- src/04-numbers.ts
+++ src/04-numbers.ts
@@ -1,6 +1,21 @@
 (()=> {
   let productPrice = 100;
   productPrice = 23;
-  
 
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 22;
+  customerAge = customerAge + 1;
+    //customerAge = customerAge + '1' // 221
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+
+  let productInStock: number;
+  console.log('productInStock', productInStock);
+  if (productInStock > 10) {
+    console.log('is greater');
+  }
+
+  let discont = parseInt('123')
+  console.log('discont', discont);
 })();
